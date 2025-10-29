c618fba8323024d9869f7bd91c4390320022377c
Eyobex1 <eyobex123@gmail.com>
2025-10-29 22:38:14 +0300

    user controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,18 @@
 import express from "express";
 import { startServer } from "./utils/startServer.js";
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
+import userRoutes from "./routes/user.route.js";
 
 const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(clerkMiddleware());
+
+// API Routes
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello from server");
