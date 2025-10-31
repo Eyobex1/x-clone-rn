5abf03706892f7ff476218f18d0d44cc2b8c6993
Eyobex1 <eyobex123@gmail.com>
2025-10-31 21:10:19 +0300

    api security added with arcjet

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,7 @@ import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
 import notificationRoutes from "./routes/notification.route.js";
+import { arcjetMiddleware } from "./middleware/arcjet.middleware.js";
 
 const app = express();
 
@@ -13,15 +14,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(clerkMiddleware());
+app.use(arcjetMiddleware);
 
 // API Routes
+app.get("/", (req, res) => res.send("server is running"));
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
 app.use("/api/notifications", notificationRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Hello from server");
+// error handling middleware
+app.use((err, req, res, next) => {
+  console.error("Unhandled error:", err);
+  res.status(500).json({ error: err.message || "Internal server error" });
 });
 
 // Start the process
