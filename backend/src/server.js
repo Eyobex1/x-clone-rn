33e9c88eef523467d0405fd8788ea5c5dff8afeb
Eyobex1 <eyobex123@gmail.com>
2025-10-31 20:29:24 +0300

    comment route added

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import { clerkMiddleware } from "@clerk/express";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js";
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use(clerkMiddleware());
 // API Routes
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello from server");
