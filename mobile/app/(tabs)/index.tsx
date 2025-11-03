3ea37244f8dc06beab171b66b1fec762bd62ef43
Eyobex1 <eyobex123@gmail.com>
2025-11-03 14:14:46 +0300

    tabs setup done and signout added

---
+++ mobile/app/(tabs)/index.tsx
@@ -0,0 +1,17 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import SignOutButton from "@/components/SignOutButton";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <View className="flex-row items-center justify-between">
+        <Text>HomeScreen</Text>
+        <SignOutButton />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
